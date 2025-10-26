922e943e5f025a247baf120fc8d9ed0f5be2907a
RomaBureacov <RomanBureacov@gmail.com>
2025-10-25 17:38:55 -0700

    additional tests

--- test/controller/reader/ScannerTest.java
+++ test/controller/reader/ScannerTest.java
@@ -35,23 +35,78 @@ public class ScannerTest {
      */
     @Test
     public void moveCommandTest() throws IllegalSyntaxException {
+        // test of the "move" keyword
         this.iScanner = new Scanner("move 0 S");
 
-        final Scanner.Token[] lExpected = {
-                this.iScanner.nextToken(),
-                this.iScanner.nextToken(),
-                this.iScanner.nextToken(),
-        };
-
-        final Scanner.Token[] lActual = {
+        Scanner.Token[] lExpected = {
                 new Scanner.Token(Type.COMMAND, "move", 4),
                 new Scanner.Token(Type.STACK, "0", 6),
                 new Scanner.Token(Type.STACK, "S", 8),
+        };
+
+        Scanner.Token[] lActual = {
+                this.iScanner.nextToken(),
+                this.iScanner.nextToken(),
+                this.iScanner.nextToken(),
         };
 
         for (int i = 0; i < lExpected.length; i++) {
             assertEquals(lExpected[i].type(), lActual[i].type(), "unequal token types");
             assertEquals(lExpected[i].image(), lActual[i].image(), "unequal token images");
         }
+
+        // test of the "stack" keyword
+        this.iScanner = new Scanner("stack 0 S");
+
+        lExpected = new Scanner.Token[]{
+                new Scanner.Token(Type.COMMAND, "stack", 4),
+                new Scanner.Token(Type.STACK, "0", 6),
+                new Scanner.Token(Type.STACK, "S", 8),
+        };
+
+        lActual = new Scanner.Token[]{
+                this.iScanner.nextToken(),
+                this.iScanner.nextToken(),
+                this.iScanner.nextToken(),
+        };
+
+        for (int i = 0; i < lExpected.length; i++) {
+            assertEquals(lExpected[i].type(), lActual[i].type(), "unequal token types");
+            assertEquals(lExpected[i].image(), lActual[i].image(), "unequal token images");
+        }
+    }
+
+    /**
+     * Test the new hand commands and its variants for recognition
+     * @throws IllegalSyntaxException if an error occurs during scanning
+     */
+    @Test
+    public void resetHandCommandTest() throws IllegalSyntaxException {
+        // test newhand variant
+        this.iScanner = new Scanner("newhand");
+
+        Scanner.Token lExpected = new Scanner.Token(Type.COMMAND, "newhand", 7);
+        Scanner.Token lActual = this.iScanner.nextToken();
+        
+        assertEquals(lExpected.type(), lActual.type(), "unequal token types");
+        assertEquals(lExpected.image(), lActual.image(), "unequal token images");
+
+        // test new variant
+        this.iScanner = new Scanner("new");
+
+        lExpected = new Scanner.Token(Type.COMMAND, "new", 3);
+        lActual = this.iScanner.nextToken();
+
+        assertEquals(lExpected.type(), lActual.type(), "unequal token types");
+        assertEquals(lExpected.image(), lActual.image(), "unequal token images");
+
+        // test reset variant
+        this.iScanner = new Scanner("reset");
+
+        lExpected = new Scanner.Token(Type.COMMAND, "reset", 5);
+        lActual = this.iScanner.nextToken();
+
+        assertEquals(lExpected.type(), lActual.type(), "unequal token types");
+        assertEquals(lExpected.image(), lActual.image(), "unequal token images");
     }
 }
